895c881277bbbf80253e41557e649a500277b909
gecrodriguezpe <gecrodriguezpe@unal.edu.co>
2024-10-28 11:59:59 -0500

    Ensayando con branches

--- prueba.py
+++ prueba.py
@@ -27,14 +27,15 @@ a = 10
 b = 5
 print(add(a, b))
 print(substract(a, b))
-print(multiply(a, b))
-print(divide(a, b))
 
 dummy_data = {
-    "key1": "value1",
+    "yeah": "value1",
     "key2": "value2",
     "key3": "value3",
     "key4": "value4",
     "key5": "value5",
     "key6": "value6",
 }
+
+lst_numbers = range(10)
+lista = [1, 2, 3, 4, 5]
